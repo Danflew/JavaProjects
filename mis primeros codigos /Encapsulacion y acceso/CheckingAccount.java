58ebb070008c36b63526a1a7aaa85af426eb968d
Danflew <142945305+Danflew@users.noreply.github.com>
2023-09-30 14:10:14 -0500

    Delete mis primeros codigos /Encapsulacion y acceso/CheckingAccount.java

--- mis primeros codigos /Encapsulacion y acceso/CheckingAccount.java
+++
@@ -1,9 +0,0 @@
-public class CheckingAccount{
-  public String name;
-  public int balance;
-
-  public CheckingAccount(String inputName, int inputBalance){
-    name = inputName;
-    balance = inputBalance;
-}
-}
